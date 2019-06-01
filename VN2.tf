0b1817e74dfedd1a3477f88a379fea71c153daa1
Luc MARTINON <martinon.luc@gmail.com>
2019-06-01 22:17:33 +0200

    Added /nexus/ in the browser

--- VN2.tf
+++ VN2.tf
@@ -52,7 +52,7 @@ resource "azurerm_network_security_group" "myterraformnsg2" {
     protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_range     = "8081"
-    source_address_prefix      = "${var.private_adress_subvn[1]}"
+    source_address_prefix      = "*"
     destination_address_prefix = "${var.private_adress_subvn[0]}"
   }
 
